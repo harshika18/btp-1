876246a889f17123c35d12306763b358d95436cb
harshika jain <harshikaj18@gmail.com>
2021-02-08 03:32:02 +0530

    input data

---
+++ example/lib/BackgroundCollectingTask.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class DataSample {
+  int temperature1;
+  int temperature2;
+  int waterpHlevel;
+  DateTime timestamp;
+
+  DataSample({
+    this.temperature1,
+    this.temperature2,
+    this.waterpHlevel,
+    this.timestamp,
+  });
+}
+
+class BackgroundCollectingTask extends Model {
+  static BackgroundCollectingTask of(
+      BuildContext context, {
+        bool rebuildOnChange = false,
+      }) =>
+      ScopedModel.of<BackgroundCollectingTask>(
+        context,
+        rebuildOnChange: rebuildOnChange,
+      );
+
+
+  final BluetoothConnection _connection;
+
+  bool inProgress;
+  List<int> _buffer = List<int>();
+
+  // @TODO , Such sample collection in real code should be delegated
+  // (via `Stream<DataSample>` preferably) and then saved for later
+  // displaying on chart (or even stright prepare for displaying).
+  // @TODO ? should be shrinked at some point, endless colleting data would cause memory shortage.
+  List<DataSample> samples = List<DataSample>();
+  String op = "0 0";
+  String valuereturn()
+  {
+    return op;
+  }
+  BackgroundCollectingTask._fromConnection(this._connection) {
+    _connection.input.listen((Uint8List data) {
+      print('ECG and PPG values: ${ascii.decode(data)}');
+      op=ascii.decode(data);
+      notifyListeners();
+    });
+    _connection.input.listen(null).onDone(() {
+      inProgress = false;
+      notifyListeners();
+      print('Disconnected!!!');
+    });
+  }
+
+
+  static Future<BackgroundCollectingTask> connect(
+      BluetoothDevice server) async {
+    final BluetoothConnection connection =
+        await BluetoothConnection.toAddress(server.address);
+    return BackgroundCollectingTask._fromConnection(connection);
+  }
+
+  void dispose() {
+    _connection.dispose();
+  }
+
+  Future<void> start() async {
+    inProgress = true;
+    _buffer.clear();
+    samples.clear();
+    notifyListeners();
+    _connection.output.add(ascii.encode('start'));
+    await _connection.output.allSent;
+  }
+
+  Future<void> cancel() async {
+    inProgress = false;
+    notifyListeners();
+    _connection.output.add(ascii.encode('stop'));
+    await _connection.finish();
+  }
+
+  Future<void> pause() async {
+    inProgress = false;
+    notifyListeners();
+    _connection.output.add(ascii.encode('stop'));
+    await _connection.output.allSent;
+  }
+
+  Future<void> reasume() async {
+    inProgress = true;
+    notifyListeners();
+    _connection.output.add(ascii.encode('start'));
+    await _connection.output.allSent;
+  }
+
+  Iterable<DataSample> getLastOf(Duration duration) {
+    DateTime startingTime = DateTime.now().subtract(duration);
+    int i = samples.length;
+    do {
+      i -= 1;
+      if (i <= 0) {
+        break;
+      }
+    } while (samples[i].timestamp.isAfter(startingTime));
+    return samples.getRange(i, samples.length);
+  }
+}
